b167d06b08b0f6eb0f221d6ddd2de1397f03542e
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2021-04-17 07:27:55 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=' https://evraz-bot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://evraz-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 if __name__ == '__main__':
